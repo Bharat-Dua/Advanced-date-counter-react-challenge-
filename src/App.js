63a56212f0f9ab9b9282e57d93047d77ec21380c
Bharat Dua <bharatdua13@gmail.com>
2024-06-28 21:58:39 +0530

    complete challenge

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./index.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <DateCounter />;
 }
 
+function DateCounter() {
+  const [inputValue, setInputValue] = useState(0);
+  const [rangeBarValue, setRangeBarValue] = useState(0);
+  const currentDate = new Date();
+  const updatedDate = new Date(currentDate);
+  updatedDate.setDate(updatedDate.getDate() + inputValue);
+
+  // const [count, setCount] = useState(0);
+  function countInc() {
+    setInputValue((c) => c + rangeBarValue);
+  }
+  function countDec() {
+    setInputValue((c) => c - 1);
+  }
+  // function rangeBarInc() {
+  //   setRangeBarValue((c) => c + 1);
+  // }
+  function reset() {
+    setInputValue(0);
+    setRangeBarValue(0);
+  }
+  return (
+    <>
+      <div className="container">
+        <input
+          type="range"
+          min={0}
+          max={10}
+          value={rangeBarValue}
+          onChange={(e) => setRangeBarValue(Number(e.target.value))}
+        />
+        {rangeBarValue}
+        <div>
+          <button onClick={countDec}>-</button>
+          <input
+            type="text"
+            value={inputValue}
+            onChange={(e) => setInputValue(Number(e.target.value))}
+          />
+          <span>{inputValue}</span>
+          <button onClick={countInc}>+</button>
+        </div>
+        <p>
+          <span>
+            {inputValue === 0
+              ? "Today is "
+              : inputValue > 0
+              ? `${inputValue} days from today is `
+              : `${Math.abs(inputValue)} days ago was`}
+          </span>
+          <span> {updatedDate.toDateString()}</span>
+        </p>
+        {inputValue > 0 ? <button onClick={reset}>Reset</button> : ""}
+      </div>
+    </>
+  );
+}
 export default App;
